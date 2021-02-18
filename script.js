b849141f1ecc38cc4d94aecea5434ac2a9042d30
Pedro Ramirez <prlara2000@gmail.com>
2021-02-17 23:22:12 -0600

    fixing the inputs

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 let day = document.getElementById( 'start' );
 let todayTitle = document.querySelectorAll( '.today' )
 let todayCalendar = document.querySelector( '.today2' );
-console.log( todayTitle );
 
 function handleDate( e ) {
-
   todayTitle[ 0 ].style.visibility = 'visible';
   todayTitle[ 1 ].style.visibility = "visible";
-
   todayCalendar.innerHTML = e.target.value;
-
 }
 
 day.onchange = handleDate;
