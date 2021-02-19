c63204ce0e0bdaf4f314e91b15a8e4573fdd95a0
Pedro Ramirez <prlara2000@gmail.com>
2021-02-18 20:25:13 -0600

    another fix

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let todayCalendar = document.querySelector( '.today2' );
 function handleDate( e ) {
   todayTitle[ 0 ].style.visibility = 'visible';
   todayTitle[ 1 ].style.visibility = "visible";
+  console.log( todayTitle );
   todayCalendar.innerHTML = e.target.value;
 }
 
