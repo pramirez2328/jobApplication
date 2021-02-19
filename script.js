bd8d8458aff21942424ff5df7a22a71909a225ec
Pedro Ramirez <prlara2000@gmail.com>
2021-02-18 22:33:49 -0600

    finally fixed the outbox

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 let day = document.getElementById( 'start' );
 let todayTitle = document.querySelectorAll( '.today' )
-let todayCalendar = document.querySelector( '.today2' );
+let todayCalendar = document.querySelector( '#today2' );
+
+let inputJobName = document.querySelector( "#inputJobName" );
+let jobName = document.querySelector( "#jobName" );
+let outputJobName = document.querySelector( "#jobName2" );
+
+let inputCrewLeader = document.querySelector( '#crewLeader' );
+let leader = document.querySelector( '#leader' );
+let outputLeader = document.querySelector( '#leader2' );
+
+let inputCrewMembers = document.querySelector( '#crewMembers' );
+let crew = document.querySelector( '#crew' );
+let outputLaborer = document.querySelectorAll( '.laborer' );
+
+console.log( inputCrewMembers );
+console.log( crew );
+console.log( outputLaborer );
+
 
 function handleDate( e ) {
   todayTitle[ 0 ].style.visibility = 'visible';
   todayTitle[ 1 ].style.visibility = "visible";
-  console.log( todayTitle );
-  todayCalendar.innerHTML = e.target.value;
+
+  todayCalendar.textContent = e.target.value;
+}
+
+function handleJobName( e ) {
+  jobName.style.visibility = 'visible';
+  outputJobName.textContent = e.target.value;
+}
+
+function handleLeaderName( e ) {
+  leader.style.visibility = 'visible';
+  outputLeader.textContent = e.target.value;
+}
+
+function handleCrewMembers( e ) {
+  crew.style.visibility = 'visible';
+  for ( let i = 0; i < outputLaborer.length; i++ ) {
+    outputLaborer[ i ].textContent = e.target.value;
+  }
 }
 
 day.onchange = handleDate;
+inputJobName.onchange = handleJobName;
+inputCrewLeader.onchange = handleLeaderName;
+inputCrewMembers.onchange = handleCrewMembers;
