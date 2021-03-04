8c37f76a79ca1a27698d98f14dc1273ce9748d0e
Pedro Ramirez <prlara2000@gmail.com>
2021-03-04 14:40:55 -0600

    finishing page 2

--- script2.js
+++ script2.js
@@ -1,23 +1,16 @@
 let todayObject2 = JSON.parse( sessionStorage.getItem( 'object1' ) );
-let compost = document.querySelector( "#compost" );
-let bark = document.querySelector( "#bark" );
-let gravel = document.querySelector( "#gravel" );
-let screenings = document.querySelector( "#screenings" );
-let soil = document.querySelector( "#soil" );
-let sod = document.querySelector( "#sod" );
-let mortar = document.querySelector( "#mortar" );
-let concrete = document.querySelector( "#concrete" );
-let cable = document.querySelector( "#cable" );
-let draintale = document.querySelector( "#draintale" );
-let edging = document.querySelector( "#edging" );
-let button2 = document.querySelector( "#button2" );
+const button2 = document.querySelector( "#button2" );
+const material = document.querySelectorAll( '.material' );
 
-console.log( todayObject2 );
 
-function handle2Input( event ) {
-  event.preventDefault( );
-  todayObject2[ "compost" ] = compost.value;
-  console.log( todayObject2 );
+function handle2Input( e ) {
+
+  for ( let i = 0; i < material.length; i++ ) {
+    if ( material[ i ].value != 0 ) {
+      todayObject2[ material[ i ].name ] = material[ i ].value;
+    }
+  }
+  sessionStorage.setItem( "object2", JSON.stringify( todayObject2 ) );
 }
 
 button2.addEventListener( "click", handle2Input );
